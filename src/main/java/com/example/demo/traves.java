dce34f746adba5b5e3a2487b7efb3988df0de2e1
Arjun Shahaji Narale <arjun.narale@ptechno.com>
2026-01-16 22:26:23 +0530

    commit

---
+++ src/main/java/com/example/demo/traves.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class traves {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemytripApplication.class, args);
+        System.out.println("/booking travels");
+
+	}
+
+}
+
