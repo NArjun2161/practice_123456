5dcfda90a93dab9faec72c31cd1a170c6d19aeeb
Arjun Narale <160503114+NArjun2161@users.noreply.github.com>
2026-01-16 22:26:17 +0530

    Update traves.java

--- src/main/java/com/example/demo/traves.java
+++ src/main/java/com/example/demo/traves.java
@@ -1,15 +1,9 @@
-package com.example.demo;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class traves {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MakemytripApplication.class, args);
         System.out.println("/booking travels");
-
 	}
 
 }
-
